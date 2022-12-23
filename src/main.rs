fd4ddcaeaa43c07e0d242605ccf3f4324a72c015
FrancoGiachetta27 <fgiachetta@frba.utn.edu.ar>
2022-12-23 18:59:22 -0300

    added board scaling for movement

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::{self, WorldInspectorPlugin};
 
+mod food;
 mod snake;
+mod utils;
 
+use food::Food;
 use snake::SnakePlugin;
+use utils::{Position, Size};
+
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
-                width: 800.0,
-                height: 800.0,
+                width: 500.0,
+                height: 500.0,
                 title: "Snake Game".to_string(),
                 ..default()
             },
@@ -19,17 +26,49 @@ fn main() {
         // Plugins
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(SnakePlugin)
+        .add_plugin(Food)
         // Systems
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(position_translation)
+                .with_system(size_scaling),
+        )
         .add_startup_system(setup)
         .run();
 }
 
-fn setup(mut commands: Commands, mut windows: ResMut<Windows>) {
-    //get window size
-    let window = windows.get_primary_mut().unwrap();
-    let (win_w, win_h) = (window.width(), window.height());
-
-    let button = win_h / 2.0;
-
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+// scale the board in order to separate it in tiles
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+// link the transform with the Position struct
+fn position_translation(windows: Res<Windows>, mut query: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+
+    let window = windows.get_primary().unwrap();
+
+    for (pos, mut transform) in query.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
 }
--- src/snake.rs
+++ src/snake.rs
@@ -1,18 +1,45 @@
-use bevy::{prelude::*, transform};
+use bevy::{prelude::*, time::FixedTimestep};
 
-#[derive(Reflect, Default, Component)]
+use crate::utils::{Position, Size};
+
+#[derive(Reflect, Default, Component, Clone, Copy, PartialEq)]
 #[reflect(Component)]
-pub struct SnakeHead;
+pub struct SnakeHead {
+    direction: Direction,
+}
+
+#[derive(Reflect, Default, Clone, Copy, PartialEq)]
+pub enum Direction {
+    #[default]
+    Up,
+    Down,
+    Right,
+    Left,
+}
 
 pub struct SnakePlugin;
-
-const SNAKE_SIZE: f32 = 25.0;
 
 impl Plugin for SnakePlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<SnakeHead>()
             .add_startup_system(snake_setup)
-            .add_system(snake_movement);
+            .add_system(get_movement.before(snake_movement))
+            .add_system_set(
+                SystemSet::new()
+                    .with_run_criteria(FixedTimestep::step(0.200))
+                    .with_system(snake_movement),
+            );
+    }
+}
+
+impl Direction {
+    pub fn opposite(self) -> Self {
+        match self {
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+            Self::Right => Self::Left,
+            Self::Left => Self::Right,
+        }
     }
 }
 
@@ -21,32 +48,45 @@ fn snake_setup(mut commands: Commands) {
         .spawn(SpriteBundle {
             sprite: Sprite {
                 color: Color::SEA_GREEN,
-                custom_size: Some(Vec2::new(SNAKE_SIZE, SNAKE_SIZE)),
-                ..Default::default()
-            },
-            transform: Transform {
-                translation: Vec3::new(0.5, 0.5, 10.0),
                 ..Default::default()
             },
             ..Default::default()
         })
-        .insert(SnakeHead)
+        .insert(SnakeHead {
+            direction: Direction::default(),
+        })
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8))
         .insert(Name::new("SnakeHaed"));
 }
 
-fn snake_movement(mut snake: Query<&mut Transform, With<SnakeHead>>, input: Res<Input<KeyCode>>) {
-    let mut snake_head = snake.single_mut();
+fn get_movement(mut snake_head: Query<&mut SnakeHead>, input: Res<Input<KeyCode>>) {
+    let mut snake_head = snake_head.single_mut();
 
-    if input.just_pressed(KeyCode::W) {
-        snake_head.translation.y += SNAKE_SIZE;
+    let dir: Direction = if input.pressed(KeyCode::W) {
+        Direction::Up
+    } else if input.pressed(KeyCode::S) {
+        Direction::Down
+    } else if input.pressed(KeyCode::A) {
+        Direction::Left
+    } else if input.pressed(KeyCode::D) {
+        Direction::Right
+    } else {
+        snake_head.direction
+    };
+
+    if dir != snake_head.direction.opposite() {
+        snake_head.direction = dir;
     }
-    if input.just_pressed(KeyCode::S) {
-        snake_head.translation.y -= SNAKE_SIZE;
-    }
-    if input.just_pressed(KeyCode::A) {
-        snake_head.translation.x -= SNAKE_SIZE;
-    }
-    if input.just_pressed(KeyCode::D) {
-        snake_head.translation.x += SNAKE_SIZE;
+}
+
+fn snake_movement(mut snake: Query<(&mut SnakeHead, &mut Position)>) {
+    for (snake_head, mut pos) in snake.iter_mut() {
+        match snake_head.direction {
+            Direction::Up => pos.y += 1,
+            Direction::Down => pos.y -= 1,
+            Direction::Left => pos.x -= 1,
+            Direction::Right => pos.x += 1,
+        }
     }
 }
